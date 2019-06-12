625e54e5a1f7f28ea4a31c04e31b4e500df9561c
Scott Levin <slevin886@gmail.com>
2019-06-12 19:34:02 -0400

    added randomization of finish location

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
-from flask import render_template, Flask
-from maze_maker import Maze, manhattan_distance
+from flask import render_template, Flask, request
+from maze_maker import Maze, manhattan_distance, Location
 from maze_search import depth_first_search, breadth_first_search, astar
-
+import numpy as np
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     m = Maze()
+    if request.method == 'POST':
+        if request.form['rand'] == 'loc':
+            rand_x = np.random.choice(np.arange(10))
+            rand_y = np.random.choice(np.arange(10)) if rand_x != 0 else np.random.choice(np.arange(1, 10))
+            m = Maze(finish=Location(x=rand_x, y=rand_y))
     final_dfs, path_dfs = depth_first_search(m.start, m.finish_line, m.frontier)
     if path_dfs is None:
         while path_dfs is None:
